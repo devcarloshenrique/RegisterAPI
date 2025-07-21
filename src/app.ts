6ecf52b5a81127197fac8f2d0a8e47bea68d999e
devcarloshenrique <ocarlosh2024@gmail.com>
2025-07-20 21:49:57 -0300

    feat: add base application structure

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { NextFunction, Request, Response } from 'express'
+import { usersRoutes } from './http/controllers/users/routes'
+import { env } from './env'
+
+const app = express()
+
+app.use(express.json());
+
+(async () => {
+  const userRoutes = await usersRoutes()
+  app.use(userRoutes);
+})()
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+   if (err instanceof Error) {
+    return res.status(409).json({ error: err.message });
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(err)
+  }
+
+  return res.status(500).json({
+    message: 'Internal server error',
+    status: 500
+  })
+})
+
+export { app }
