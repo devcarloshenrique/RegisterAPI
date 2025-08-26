3717bb422f11b10d50361b3f67716d1f5b906d67
devcarloshenrique <ocarlosh2024@gmail.com>
2025-08-26 17:44:52 -0300

    feat(docs): Implement Swagger for API documentation

---
+++ src/docs/swagger.ts
@@ -0,0 +1,36 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerPaths } from './swagger-config';
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Processamento de Arquivos',
+      version: '1.0.0',
+      description: 'Documentação da API com Express e Swagger',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3333/api/',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          description: 'Enter the JWT token in the format: Bearer <token>'
+        }
+      }
+    },
+    paths: swaggerPaths,
+  },
+  apis: ['src/http/controllers/**/*.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export { swaggerSpec, swaggerUi };
+
