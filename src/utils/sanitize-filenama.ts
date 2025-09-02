839d74b417ec3135b25352106a3472bcf181a31e
devcarloshenrique <ocarlosh2024@gmail.com>
2025-09-01 21:01:52 -0300

    feat: add sanitizeOriginalFileName

--- src/utils/sanitize-filenama.ts
+++ src/utils/sanitize-filenama.ts
@@ -1,7 +1,14 @@
+export function sanitizeOriginalFilename(filename: string): string {
+  return filename
+    .replace(/[^a-zA-Z0-9\s_.\-()]/g, '')   // Remove invalid characters
+    .replace(/\s+/g, ' ')                   // Replace multiple spaces with a single space
+    .trim();                                // Trim whitespace from both ends
+}
+
 export function sanitizeFileName(filename: string): string {
   return filename
-    .normalize('NFD') // Decompõe acentos (ex: á → a + ́)
-    .replace(/[\u0300-\u036f]/g, '') // Remove marcas de acento
-    .replace(/[^a-zA-Z0-9._-]/g, '_') // Substitui caracteres inválidos por "_"
-    .replace(/\s+/g, '_'); // Substitui espaços por "_"
+    .normalize('NFD')                  // Decompose accents (e.g., á → a + ´)
+    .replace(/[\u0300-\u036f]/g, '')   // Remove accent marks
+    .replace(/[^a-zA-Z0-9._-]/g, '_')  // Replace invalid chars with "_"
+    .replace(/\s+/g, '_');             // Replace spaces with "_"
 }
