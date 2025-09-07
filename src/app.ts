b42e8ee39f52f76303d0a4b36011d89f66ff0532
devcarloshenrique <ocarlosh2024@gmail.com>
2025-09-06 22:20:38 -0300

    refactor: Improve asynchronous request handling in app and server

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,16 @@ import { swaggerSpec, swaggerUi } from './docs/swagger'
 import { createMainRouter } from './http/routes'
 import { errorHandler } from './http/middlewares/error-handler'
 
-const app = express()
+async function bootstrap() {
+  const app = express()  
+  app.use(express.json());
 
-app.use(express.json());
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-(async () => {
-  const apiRouter = await createMainRouter();
-  app.use('/api', apiRouter);
-  
+  app.use('/api', await createMainRouter());
   app.use(errorHandler)
-})();
+  
+  return { app }
+}
 
-export { app }
+export { bootstrap }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { app } from "./app";
+import { bootstrap } from "./app";
 import { env } from "./env";
 
-app.listen({
-	host: '0.0.0.0',
-	port: env.PORT,
-}, () => {
-	console.log(`Server running on port ${env.PORT}`)
+bootstrap().then(({ app }) => {
+	app.listen({
+		host: '0.0.0.0',
+		port: env.PORT,
+	}, () => {
+		console.log(`Server running on port ${env.PORT}`)
+	})
 })
