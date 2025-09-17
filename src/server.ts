84c8cd8ec305c3836039db316a0e8668a9358c08
devcarloshenrique <ocarlosh2024@gmail.com>
2025-09-16 21:33:30 -0300

    refactor: container port correction

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ bootstrap().then(({ app }) => {
 		host: '0.0.0.0',
 		port: env.PORT,
 	}, () => {
-		console.log(`Server running on port ${env.PORT}`)
+		console.log(
+			`API: http://localhost:${env.PORT}\n` +
+			`Documentação Swagger: http://localhost:${env.PORT}/docs\n` +
+			`Dashboard Bull Board: http://localhost:${env.PORT}/admin/queues`
+		)
 	})
 })
